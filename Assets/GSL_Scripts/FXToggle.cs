ba633fbb90e374c410ff1bb11c257cb7c7727985
Danail Georgiev <i425982@fhict.nl>
2022-06-08 12:19:00 +0200

    Fixed sprites for sound button

---
+++ Assets/GSL_Scripts/FXToggle.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXToggle : MonoBehaviour
+{
+    List<AudioSource> sources = new List<AudioSource>();
+    bool muted = false;
+
+    void Start()
+    {
+        // get all audio sources
+        object[] obj = GameObject.FindObjectsOfType(typeof(GameObject));
+        foreach (object o in obj)
+        {
+            GameObject g = (GameObject)o;
+            if(g.GetComponent<AudioSource>() != null && g.name != "Music")
+            {
+                sources.Add(g.GetComponent<AudioSource>());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // refresh list
+        sources.Clear();
+        object[] obj = GameObject.FindObjectsOfType(typeof(GameObject));
+        foreach (object o in obj)
+        {
+            GameObject g = (GameObject)o;
+            if (g.GetComponent<AudioSource>() != null && g.name != "Music")
+            {
+                sources.Add(g.GetComponent<AudioSource>());
+            }
+        }
+    }
+
+    public void ToggleSounds()
+    {
+        if(muted)
+        {
+            foreach (AudioSource source in sources)
+            {
+                source.volume = 1;
+            }
+            muted = false;
+        }
+        else
+        {
+            foreach (AudioSource source in sources)
+            {
+                source.volume = 0;
+            }
+            muted = true;
+        }
+    }
+}
